16b02aba08c8c8c4db0e537e18c199bab28a46da
AishwaryaPM11 <aishwaryamulagund0@gmail.com>
2025-11-21 12:18:09 +0530

    Update cie.py

--- cie.py
+++ cie.py
@@ -1,23 +1,18 @@
 import sys
 
 if len(sys.argv) != 2:
-    print("Usage: python script.py <basic_salary>")
-else:
-    hra=float(sys.argv[1])
+     hra=float(sys.argv[1])
     ta=float(sys.argv[2])
     da=float(sys.argv[3])
- 
-
-    basic_salary = float(sys.argv[1])
-    
+else:
     hra = 0.20 * basic_salary
     ta = 0.10 * basic_salary
-    da = 0.18 * basic_salary
+    da = 0.18 * basic_salary  
+    
+gross_salary = basic_salary + hra + ta + da    
 
-    gross_salary = basic_salary + hra + ta + da    
-
-    print("Basic Salary: {basic_salary}")
-    print("HRA: {hra}")
-    print("TA: {ta}")
-    print("DA: {da}")
-    print("Gross Salary: {gross salary}")
+print("Basic Salary: {basic_salary}")
+print("HRA: {hra}")
+print("TA: {ta}")
+print("DA: {da}")
+print("Gross Salary: {gross salary}")
