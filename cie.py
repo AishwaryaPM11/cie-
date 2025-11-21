32067a5e89ea65522255734cbbecc9e927312a57
AishwaryaPM11 <aishwaryamulagund0@gmail.com>
2025-11-21 11:31:55 +0530

    save this

--- cie.py
+++ cie.py
@@ -4,17 +4,11 @@ import sys
 if len(sys.argv) != 2:
     print("Usage: python script.py <basic_salary>")
     sys.exit(1)
-
-
-try:
     basic_salary = float(sys.argv[1])
-except ValueError:
-    print("Error: Basic salary must be a number.")
-    sys.exit(1)
-
-hra = 0.20 * basic_salary
-ta = 0.10 * basic_salary
-da = 0.18 * basic_salary
+else:
+    hra = 0.20 * basic_salary
+    ta = 0.10 * basic_salary
+    da = 0.18 * basic_salary
 
 gross_salary = basic_salary + hra + ta + da
 
